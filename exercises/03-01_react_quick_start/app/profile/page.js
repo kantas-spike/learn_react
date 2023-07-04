2f58fd926d6beebb0353434d0aaa4cb95ebaaf40
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-04 16:42:19 +0900

    03-01: 条件により表示項目を変更可能に

--- exercises/03-01_react_quick_start/app/profile/page.js
+++ exercises/03-01_react_quick_start/app/profile/page.js
@@ -1,10 +1,19 @@
+import MyButton from "@/components/button"
+
 const user = {
     name: 'Hedy Lamarr',
     imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 90
+    imageSize: 90,
+    isLoggedIn: true
 }
 
 export default function Profile() {
+    let content
+    if (user.isLoggedIn) {
+        content = <button className="border rounded-md px-2 py-1">ログアウト</button>
+    } else {
+        content = <MyButton />
+    }
     return (
         <div className="m-8">
         <h1 className="text-2xl mb-3">{user.name}</h1>
@@ -16,6 +25,13 @@ export default function Profile() {
                 height: user.imageSize
               }}
         />
+        {content}
+        <p className="mt-8 text-xs">{ user.isLoggedIn ? (
+            <span>ログイン中</span>
+            ) : (
+            <span>未ログイン</span>
+            )
+            }</p>
         </div>
     )
 }

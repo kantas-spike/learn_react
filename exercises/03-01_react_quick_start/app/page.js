bc7f1b7832296e8ec17ab4991a6eb6cba184ca70
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-04 17:09:09 +0900

    03-01: Homeにページ一覧を表示

--- exercises/03-01_react_quick_start/app/page.js
+++ exercises/03-01_react_quick_start/app/page.js
@@ -1,10 +1,25 @@
 import MyButton from "@/components/button"
+import Link from "next/link"
+
+const pages = [
+  { title: 'About', path: '/about' },
+  { title: 'Profile', path: '/profile' },
+  { title: 'Shopping List', path: '/shopping_list' },
+]
 
 export default function Home() {
+  const listItems = pages.map( (page, idx) => <li key={idx}>
+    <Link className="underline underline-offset-4 text-blue-400" href={page.path}>{page.title}</Link>
+  </li>)
+
   return (
-    <div>
+    <div className="m-6">
       <h1>Welcom to my app</h1>
       <MyButton />
+
+      <ul className="list-disc list-inside mt-6">
+        {listItems}
+      </ul>
     </div>
   )
 }

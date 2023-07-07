84360c307cf61d9bf3c08be0f7a3a5618cd57dc8
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-08 02:03:58 +0900

    03-02: 'マス'をインタラクティブに
    試しにクリック時にコンソールログを出力

--- exercises/03-02_react_tutorial_tic-tac-toe/app/components/square.js
+++ exercises/03-02_react_tutorial_tic-tac-toe/app/components/square.js
@@ -1,3 +1,9 @@
+'use client'
+
 export default function Square({value}) {
-    return <button className="border w-8 h-8">{value}</button>
+    function handleClick() {
+        console.log('clicked!')
+    }
+
+    return <button onClick={handleClick} className="border w-8 h-8">{value}</button>
 }

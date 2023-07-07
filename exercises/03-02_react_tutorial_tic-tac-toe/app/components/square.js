29ccca545ae2e44591cc64b9daea0547d747b972
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-08 02:49:16 +0900

    03-02: 'マス'に状態を持たせず、盤が全`マス`の状態を持つように変更

--- exercises/03-02_react_tutorial_tic-tac-toe/app/components/square.js
+++ exercises/03-02_react_tutorial_tic-tac-toe/app/components/square.js
@@ -1,13 +1,5 @@
 'use client'
 
-import { useState } from "react"
-
-export default function Square() {
-    const [value, setValue] = useState(null)
-
-    function handleClick() {
-        setValue('X')
-    }
-
-    return <button onClick={handleClick} className="border w-8 h-8">{value}</button>
+export default function Square({value, onSquareClick}) {
+    return <button onClick={onSquareClick} className="border w-8 h-8">{value}</button>
 }

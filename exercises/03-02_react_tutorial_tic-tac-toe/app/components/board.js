94e3090e6536a7a83121af3e0aebc5ed28c2b4e2
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-08 11:44:15 +0900

    03-02: 勝敗判定ロジックを独立したファイルへ移動
    `@/lib/game_logic`でインポートできるように設定

--- exercises/03-02_react_tutorial_tic-tac-toe/app/components/board.js
+++ exercises/03-02_react_tutorial_tic-tac-toe/app/components/board.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import Square from "./square"
+import { calculateWinner } from "@/lib/game_logic"
 
 export default function Board() {
     const [xIsNext, setXIsNext] = useState(true)
@@ -49,26 +50,4 @@ export default function Board() {
             </div>
         </div>
     )
-}
-
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i]
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a]
-        }
-    }
-
-    return null
 }

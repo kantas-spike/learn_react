e36e7eb3eca3868a96be6904c1fb04c2b007fbc6
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-08 12:07:27 +0900

    03-02: 引き分け表示時のレイアウトのずれを修正

--- exercises/03-02_react_tutorial_tic-tac-toe/app/page.js
+++ exercises/03-02_react_tutorial_tic-tac-toe/app/page.js
@@ -2,7 +2,7 @@ import Board from "./components/board"
 
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex flex-col justify-center items-center">
       <Board></Board>
     </div>
   )

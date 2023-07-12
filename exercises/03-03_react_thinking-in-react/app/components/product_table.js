c91a20b53706e22b1557a489a3556e38614aabfb
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-12 21:13:51 +0900

    03-03: interactivity版のReact UIを作成

--- exercises/03-03_react_thinking-in-react/app/components/product_table.js
+++ exercises/03-03_react_thinking-in-react/app/components/product_table.js
@@ -1,8 +1,14 @@
 import ProductCategoryRow from "./product_category_row"
 import ProductRow from "./product_row"
 
-export default function ProductTable({products}) {
+export default function ProductTable({products, filterText, inStockOnly}) {
     const grouped_products = products.reduce((group, item) => {
+        if (filterText.length > 0 && !item.name.toLowerCase().includes(filterText.toLowerCase())) {
+            return group
+        }
+        if (inStockOnly && !item.stocked) {
+            return group
+        }
         const category = item.category
         group[category] ||= []
         group[category].push(item)
@@ -10,10 +16,12 @@ export default function ProductTable({products}) {
     }, {})
 
     const rows = []
+    let idx = 0
     for (const [category] of Object.entries(grouped_products)) {
-        rows.push(<ProductCategoryRow category={category}></ProductCategoryRow>)
+        rows.push(<ProductCategoryRow key={category} category={category}></ProductCategoryRow>)
         for (const product of grouped_products[category]) {
-            rows.push(<ProductRow product={product}></ProductRow>)
+            idx += 1
+            rows.push(<ProductRow key={idx} product={product}></ProductRow>)
         }
     }
 
--- exercises/03-03_react_thinking-in-react/app/components/search_bar.js
+++ exercises/03-03_react_thinking-in-react/app/components/search_bar.js
@@ -1,9 +1,13 @@
-export default function SearchBar() {
+export default function SearchBar({filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange}) {
     return (
         <div className="py-4 px-8 mb-2">
-            <input type="text" placeholder="Search..." className="mb-2 pl-1"></input>
+            <input type="text" value={filterText}
+                onChange={(e) => onFilterTextChange(e.target.value)}
+                placeholder="Search..." className="mb-2 pl-1 text-black"></input>
             <div>
-                <input name="inStock" id="inStock" type="checkbox" className="mr-2"></input>
+                <input name="inStock"  checked={inStockOnly}
+                    onChange={(e) => onInStockOnlyChange(e.target.checked)}
+                    id="inStock" type="checkbox" className="mr-2"></input>
                 <label htmlFor="inStock">Only show products in stock</label>
             </div>
         </div>

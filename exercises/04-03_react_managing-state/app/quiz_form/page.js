41bab01b8b59c66c311d1a5f5700f7ac01b68750
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-15 22:26:09 +0900

    04-03: quiz_formを追加

---
+++ exercises/04-03_react_managing-state/app/quiz_form/page.js
@@ -0,0 +1,9 @@
+import QuizForm from "@/components/quiz_form";
+
+export default function QuizFormPage(){
+    return (
+        <div className="m-4">
+            <QuizForm></QuizForm>
+        </div>
+    )
+}

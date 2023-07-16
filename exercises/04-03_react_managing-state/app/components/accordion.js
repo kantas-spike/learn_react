2b4138339ee955c05d4a73da25f769f009a8b12c
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-16 10:40:11 +0900

    04-03: eslintのエラー(eslintreact/no-unescaped-entities)対応
    `'`や`"`を実体参照に置換

--- exercises/04-03_react_managing-state/app/components/accordion.js
+++ exercises/04-03_react_managing-state/app/components/accordion.js
@@ -11,7 +11,7 @@ export default function Accordion(){
                 title="About"
                 isActive={activeIndex === 0}
                 onShow={()=>setActiveIndex(0)}>
-                With a population of about 2 million. Almaty is Kazakhstan's largest city.
+                With a population of about 2 million. Almaty is Kazakhstan&apos;s largest city.
                 From 1929 to 1997, it was its capital city.
             </Panel>
             <Panel
@@ -19,7 +19,7 @@ export default function Accordion(){
                 isActive={activeIndex === 1}
                 onShow={()=>setActiveIndex(1)}>
                 The name comes from <span>алма</span>,
-                the Kazakh word for "apple" and is often translated as "full of apples".
+                the Kazakh word for &quot;apple&quot; and is often translated as &quot;full of apples&quot;.
                 In fact, the region surrounding Almaty is thought to be the ancestral home of the apple,
                 and the wild Malus sieversii is considered a likely candidate for the ancestor of the modern domestic apple.
             </Panel>

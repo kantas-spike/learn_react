1964abe5a1dc9c9586871363265aac50ce2a94d2
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-17 13:18:03 +0900

    04-03: sectionはcontextの利用と提供の両方を行うように修正

--- exercises/04-03_react_managing-state/components/section.js
+++ exercises/04-03_react_managing-state/components/section.js
@@ -1,11 +1,13 @@
 'use client'
 
+import { useContext } from "react";
 import { LevelContext } from "./level_context";
 
-export default function Section({level, children}){
+export default function Section({children}){
+    const level = useContext(LevelContext)
     return (
         <section className="m-2 py-2 px-2 border rounded-md">
-            <LevelContext.Provider value={level}>
+            <LevelContext.Provider value={level + 1}>
                 {children}
             </LevelContext.Provider>
         </section>

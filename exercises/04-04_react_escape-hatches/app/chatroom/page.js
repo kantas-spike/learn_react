bc7d78712056383b04baa8b1c3f986df0063963b
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-19 13:22:56 +0900

    04-04: chatroomへの接続時にテーマ付き通知を表示(ただしバグ版)

--- exercises/04-04_react_escape-hatches/app/chatroom/page.js
+++ exercises/04-04_react_escape-hatches/app/chatroom/page.js
@@ -4,9 +4,10 @@ import ChatRoom from "@/components/chatroom"
 import { useState } from "react"
 
 export default function ChatRoomPage(){
+    const [isDark, setIsDark] = useState(false)
     const [roomId, setRoomId] = useState('general')
     return (<main>
-        <label>
+        <label className="block">
             choose the chat room: {' '}
             <select
                 value={roomId}
@@ -16,7 +17,13 @@ export default function ChatRoomPage(){
                     <option value="music">music</option>
             </select>
         </label>
+        <label htmlFor="chk_dark" className="m-4 block">
+            <input type="checkbox" name="chk_dark" id="chk_dark"
+                checked={isDark}
+                onChange={e => setIsDark(e.target.checked)} />
+                Use dark theme
+        </label>
         <hr className="m-4"/>
-        <ChatRoom roomId={roomId}></ChatRoom>
+        <ChatRoom roomId={roomId} theme={isDark ? 'dark' : 'light'}></ChatRoom>
     </main>)
 }
--- exercises/04-04_react_escape-hatches/components/chatroom.js
+++ exercises/04-04_react_escape-hatches/components/chatroom.js
@@ -1,13 +1,17 @@
 import createConnection from "@/components/chat"
 import { useEffect } from "react"
+import showNotification from "./notifications"
 
 const serverUrl = 'https://localhost:1234'
 
-export default function ChatRoom({roomId}){
+export default function ChatRoom({roomId, theme}){
     useEffect(() => {
         const connection = createConnection(roomId, serverUrl)
+        connection.on('connected', () => {
+            showNotification('Connected!', theme)
+        })
         connection.connect()
         return () => connection.disconnect()
-    }, [roomId])
+    }, [roomId, theme])
     return (<h1>Welcom to the chat!</h1>)
 }

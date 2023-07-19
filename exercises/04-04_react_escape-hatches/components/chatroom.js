06dadee03d050c7b1f51b971d668debd1c68d714
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-19 14:35:02 +0900

    04-04: オブジェクトをuseEffect内で生成することで毎回実行を防ぐ

--- exercises/04-04_react_escape-hatches/components/chatroom.js
+++ exercises/04-04_react_escape-hatches/components/chatroom.js
@@ -5,14 +5,15 @@ import showNotification from "./notifications"
 const serverUrl = 'https://localhost:1234'
 
 export default function ChatRoom({roomId, theme}){
-    const options = {
-        serverUrl: serverUrl,
-        roomId: roomId
-    }
     useEffect(() => {
+        const options = {
+            serverUrl: serverUrl,
+            roomId: roomId
+        }
+
         const connection = createConnection(options)
         connection.connect()
         return () => connection.disconnect()
-    }, [options])
+    }, [roomId])
     return (<h1>Welcom to the chat!</h1>)
 }

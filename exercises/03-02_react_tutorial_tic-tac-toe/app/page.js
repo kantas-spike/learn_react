7cfb67e37c6ce655cbc1ea1bc03e161daf7bf3f9
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-23 01:23:04 +0900

    feat: iframeで埋め込み時にappの高さ変更を通知 (fix: #13)

--- exercises/03-02_react_tutorial_tic-tac-toe/app/page.js
+++ exercises/03-02_react_tutorial_tic-tac-toe/app/page.js
@@ -1,8 +1,34 @@
+'use client'
+
 import Game from "@/components/game"
+import { useEffect, useRef } from "react"
 
 export default function Home() {
+  const divRef = useRef(null)
+  useEffect(() => {
+    if (window.self !== window.top) {
+      console.log("in frame")
+      const resizeObserver = new ResizeObserver(entries => {
+        for (let entry of entries) {
+          if (entry.borderBoxSize[0]) {
+            console.log(entry.borderBoxSize[0])
+            console.log(`h: ${entry.borderBoxSize[0].blockSize}, w: ${entry.borderBoxSize[0].inlineSize}`)
+            window.parent.postMessage({ height: entry.borderBoxSize[0].blockSize }, "*")
+          }
+        }
+      })
+      resizeObserver.observe(divRef.current)
+    } else {
+      console.log("not in frame")
+    }
+    return () => {
+      console.log("resizeObserver.disconnect()...")
+      resizeObserver.disconnect()
+    }
+  }, [])
+
   return (
-    <div className="">
+    <div ref={divRef} className="m-4 p-4 border rounded-md">
       <Game/>
     </div>
   )
